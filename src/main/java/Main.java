4d722e18ffc80d3edeb153f7cb1d12f42adc486b
Sergey Igoshin <macinq@gmail.com>
2020-07-05 15:07:25 +0400

    Выведены все (наверное) строки текста.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import somepackage.Other;
 import somepackage.Some;
+import somethinggreen.AnnotationForExaple;
 import somethinggreen.Contract;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -11,10 +11,10 @@ public class Main {
     public static void main(String[] args) {
 
         HashMap<String,Object> map = new HashMap<>();
-map.put("key1", "value");
-map.put("key2", "value");
-map.put("key3", "value");
-map.put("key4", "value");
+        map.put("key1", "value");
+        map.put("key2", "value");
+        map.put("key3", "value");
+        map.put("key4", "value");
 
         /*LinkedList list =
                 new LinkedList();
@@ -35,5 +35,15 @@ map.put("key4", "value");
         Contract.staticMethod("");
 
         System.out.println(map);
+
+        Some some = new Some("ku");
+        some.soTellMeAboutInterfaces();
+        System.out.println(some.getДаТакТожеМожно());
+        System.out.println(some.toString());
+        System.out.println(Some.class.getAnnotation(AnnotationForExaple.class).description());
+        System.out.println(some.count());
+        Other.saySomthing();
+
+
     }
 }
